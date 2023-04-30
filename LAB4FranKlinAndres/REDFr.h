d710c3bfa030a4c3a91cdf34c4cf17a1afcf0bf4
FrAndres7777 <fanaconaperafan@gmail.com>
2023-04-29 21:20:13 -0500

    Funciones De la CLASE REDFR

--- LAB4FranKlinAndres/REDFr.h
+++ LAB4FranKlinAndres/REDFr.h
@@ -14,12 +14,25 @@ public:
     ~red();
     void agregar_Enrutador(const string &router_name);
     void eliminar_Enrutador(const string &router_name);
-
+    void Conectar2Enrutadores(const string &name1,const string &name2,int cost);
+    void DEsconectar2Enrutadores(const string &name1,const string &name2,int cost);
+    int Costo(const string &salida, const string &llegada);
+    void MejorCamino(const string &salida, const string &llegada);
+    bool comprobar_Enrutador(const string &router_name);
+    void AlgoritmoDijkstra(const string &nodoInicial);
+    void ActualizarTabla();
+    void TablaConexionesRed();
+    void TablaCostosRed();
+    void TablaConexionesEnrutador(const string &nodo);
+    void TablaCostosEnrutador(const string &nodo);
+    void OrdenarLista();
+    bool ComprobarConexion(const string &nodo,const string &nodo2);
+    void LeerTexto(const string &nombretexto);
+    void GenerarRedAleatoria();
 
     list<enrutador> getN_enrutadores() const;
     void setN_enrutadores(const list<enrutador> &value);
 };
-
 
 
 
